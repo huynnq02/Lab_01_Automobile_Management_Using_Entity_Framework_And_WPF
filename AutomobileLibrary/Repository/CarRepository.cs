1deae3218232c19a14f80aced0ea1640b0aeb881
huyy802 <20521401@gm.uit.edu.vn>
2023-09-28 14:35:47 +0700

    feat: done

---
+++ AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,14 @@
+﻿using AutomobileLibrary.DataAccess;
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public Car GetCarByID(int carId) => CarManagement.Instance.GetCarByID(carId);
+        public IEnumerable<Car> GetCars() => CarManagement.Instance.GetCarList();
+        public void InsertCar(Car car) => CarManagement.Instance.AddNew(car);
+        public void DeleteCar(Car car) => CarManagement.Instance.Remove(car);
+        public void UpdateCar(Car car) => CarManagement.Instance.Update(car);
+
+    }
+}
